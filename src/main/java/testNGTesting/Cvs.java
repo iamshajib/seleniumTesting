2fedcdac1f1ec33c65969b589f85bdb9a4c4056f
iamshajib <iamsajib1@gmail.com>
2022-01-09 13:33:13 -0500

    CVS Active/ Inactive links

--- src/main/java/testNGTesting/Cvs.java
+++ src/main/java/testNGTesting/Cvs.java
@@ -1,9 +1,13 @@
 package testNGTesting;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
@@ -53,10 +57,47 @@ public class Cvs {
 		
 		//Case is failing. Unable to locate ChroPath generated link is also. Tried Rel & Abs both
 		driver.findElement(By.xpath("//body/div[@id='root']/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/main[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/nav[1]/div[1]/ul[1]/li[4]")).click();
-		
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 	}
 	
+	//3rd case | number of link
+	@Test
+	public void linksCount() {
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		int linksCount = links.size();
+		
+		System.out.println("Total number of links on this page is " + linksCount);
+		
+		for(int i = 1; i < linksCount; i++) {
+			WebElement storage = links.get(i);
+			String linksAttribute = storage.getAttribute("href");
+			System.out.println(linksAttribute);
+			verifyLinkActive(linksAttribute);
+		}
+	}
 	
+	// 3rd case extends | broken links
+	private static void verifyLinkActive(String linksAtt) {
+		try {
+			URL linksAttribute = new URL(linksAtt);
+			HttpURLConnection httpURLConnect = (HttpURLConnection)linksAttribute.openConnection();
+			httpURLConnect.setConnectTimeout(3000);
+			httpURLConnect.connect();
+			
+			if(httpURLConnect.getResponseCode() == 200) {
+				System.out.println(linksAtt + " - " + httpURLConnect.getResponseMessage());
+			}
+			
+			if(httpURLConnect.getResponseCode() == HttpURLConnection.HTTP_NOT_FOUND) {
+				System.out.println(linksAtt + " - " + httpURLConnect.getResponseCode() + " - " + HttpURLConnection.HTTP_NOT_FOUND);
+			}
+		} 
+		catch(Exception e) {
+			
+		}
+		
+	}
+
 	
 	@AfterMethod
 	public void browserClose() {
