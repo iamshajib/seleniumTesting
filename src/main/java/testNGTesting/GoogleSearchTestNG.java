39f413228c37701222aab3d70c11509a0ba2b031
iamshajib <iamsajib1@gmail.com>
2022-01-08 23:09:47 -0500

    testNG instllatino & first file creation

---
+++ src/main/java/testNGTesting/GoogleSearchTestNG.java
@@ -0,0 +1,64 @@
+package testNGTesting;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class GoogleSearchTestNG {
+	
+	static WebDriver driver;
+	
+	
+	//browser open method
+	@BeforeMethod
+	public void browserOpen() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver",
+				"/Users/shajib/Documents/study/QA/development/java/Exercise/Shajib/chromedriver");
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+//		driver.manage().window().maximize();
+		Thread.sleep(3000);
+	}
+	
+//	@Test
+//	public void goToGoogle() throws InterruptedException {
+//		driver.get("https://www.google.com/");
+//		Thread.sleep(3000);
+//	}
+
+	
+	@Test
+	public void search() throws InterruptedException {
+		driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Amex");
+		Thread.sleep(3000);
+	}
+	
+	@Test
+	public void clickOnResult() throws InterruptedException {
+		List<WebElement> searchResults = driver.findElements(By.xpath("//ul[@role='listbox']//li/descendant::div[@class='wM6W7d']"));
+		System.out.println(searchResults.size());
+		
+		for(int i = 0; i < searchResults.size(); i++) {
+			String searchIteams = searchResults.get(i).getText();
+			if(searchIteams.contains("AmEx")) {
+				searchResults.get(i).click();
+				break;
+			}
+		}
+		Thread.sleep(1000);
+	}
+
+	
+	
+	@AfterMethod
+	public void closeBrowser() {
+		driver.quit();
+	}
+
+}
