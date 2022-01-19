c3d15c26f419a87cdfa77146bdb45376db250392
iamshajib <iamsajib1@gmail.com>
2022-01-19 08:33:20 -0500

    before creating new branch

---
+++ src/test/java/testNGTesting/BurteForceNewTest.java
@@ -0,0 +1,19 @@
+package testNGTesting;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class BurteForceNewTest {
+  @Test
+  public void f() {
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+}
